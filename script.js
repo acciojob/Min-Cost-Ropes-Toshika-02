2b2a5b8e858cafe17d8459f3618b3b7d3cade17e
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-26 14:11:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let sum = 0
+	for(let i=0; i<arr.length; i++){
+		if(sum === arr.length){
+			
+		}
+		 
+	}
+	 return mincost;
   
 }
 
